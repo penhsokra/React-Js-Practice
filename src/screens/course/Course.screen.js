76074fbbdcdf234fcb063639b14030b4e315bf31
penhsokra <penhsokra@gmail.com>
2022-08-25 14:25:29 +0900

    xxx

--- src/screens/course/Course.screen.js
+++ src/screens/course/Course.screen.js
@@ -1,7 +1,83 @@
-import React from 'react';
-
+import { useEffect, useState } from 'react';
+import service from '../../service/service';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import './Course.css';
 function Course() {
-  return <div>Course.screen</div>;
+  const [show, setShow] = useState(false);
+
+
+  useEffect(() => {
+    getCourse();
+  }, []);
+
+  const getCourse = (event, user) => {
+    setShow(true);
+    event.preventDefault();
+    setLoginMessage('');
+    service('POST', '/api/course', {}).then(function (response) {
+      setShow(false);
+      console.log(response.message);
+      if (response.error) {
+
+      } else {
+
+      }
+    });
+  };
+  return (
+    <div className='auth_wrap'>
+      <div className={show ? 'loading' : 'loading hide'}>
+        <svg width='100' height='100' viewBox='0 0 100 100'>
+          <polyline
+            className='line-cornered stroke-still'
+            points='0,0 100,0 100,100'
+            strokeWidth='10'
+            fill='none'
+          ></polyline>
+          <polyline
+            className='line-cornered stroke-still'
+            points='0,0 0,100 100,100'
+            strokeWidth='10'
+            fill='none'
+          ></polyline>
+          <polyline
+            className='line-cornered stroke-animation'
+            points='0,0 100,0 100,100'
+            strokeWidth='10'
+            fill='none'
+          ></polyline>
+          <polyline
+            className='line-cornered stroke-animation'
+            points='0,0 0,100 100,100'
+            strokeWidth='10'
+            fill='none'
+          ></polyline>
+        </svg>
+      </div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>CODE</th>
+          <th>STOCK</th>
+          <th>CAP</th>
+          <th>INCH</th>
+          <th>BOX TYPE</th>
+        </tr>
+      <thead>
+      <tbody>
+        <tr>
+          <td>CES-9000</td>
+          <td>50mt</td>
+          <td>9mm</td>
+          <td>1/2"</td>
+          <td>Kangal / Coil</td>
+      </tbody>
+    <table/>
+
+    </div>
+  );
 }
 
 export default Course;

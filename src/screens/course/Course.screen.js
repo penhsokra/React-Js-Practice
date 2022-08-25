45c7de31df2b0275ed8b0bfb92e22b4df9c2496b
penhsokra <penhsokra@gmail.com>
2022-08-25 15:47:35 +0900

    xxxx

--- src/screens/course/Course.screen.js
+++ src/screens/course/Course.screen.js
@@ -23,35 +23,7 @@ function Course() {
   };
   return (
     <div className=''>
-      <div className={show ? 'loading' : 'loading hide'}>
-        <svg width='100' height='100' viewBox='0 0 100 100'>
-          <polyline
-            className='line-cornered stroke-still'
-            points='0,0 100,0 100,100'
-            strokeWidth='10'
-            fill='none'
-          ></polyline>
-          <polyline
-            className='line-cornered stroke-still'
-            points='0,0 0,100 100,100'
-            strokeWidth='10'
-            fill='none'
-          ></polyline>
-          <polyline
-            className='line-cornered stroke-animation'
-            points='0,0 100,0 100,100'
-            strokeWidth='10'
-            fill='none'
-          ></polyline>
-          <polyline
-            className='line-cornered stroke-animation'
-            points='0,0 0,100 100,100'
-            strokeWidth='10'
-            fill='none'
-          ></polyline>
-        </svg>
-      </div>
-
+      <div class="loader hide"></div>
     <table>
     <colgroup>
       <col style={{width:50}}/>

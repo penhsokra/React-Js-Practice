b9cd44daacae86ec634470820ac16b3699d747d3
penhsokra <penhsokra@gmail.com>
2022-08-26 16:01:14 +0900

    page not found add

--- src/screens/pageNotFound/PageNotFound.screen.js
+++ src/screens/pageNotFound/PageNotFound.screen.js
@@ -4,14 +4,6 @@ function PageNotFound() {
   let navigate = useNavigate();
   return (
 <div className='clouds'>
-  <div id="clouds">
-      <div className="cloud x1"></div>
-      <div className="cloud x1_5"></div>
-      <div className="cloud x2"></div>
-      <div className="cloud x3"></div>
-      <div className="cloud x4"></div>
-      <div className="cloud x5"></div>
-  </div>
   <div className='c'>
         <div className='_404'>404</div>
         <hr/>

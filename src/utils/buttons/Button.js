cfcb124d708691733e7223fdfb3fda9948bca8ae
penhsokra <penhsokra@gmail.com>
2022-08-26 17:19:53 +0900

    xx

--- src/utils/buttons/Button.js
+++ src/utils/buttons/Button.js
@@ -1,16 +1,12 @@
 import './Button.css'
-const GREENBUTTON = (props) => {
+export const GREENBUTTON = (props) => {
   return (
     <button className='button button2' onClick={props.onClick}>{props.title}</button>
   );
 }
 
-const REDBUTTON = (props) => {
+export const REDBUTTON = (props) => {
     return (
         <button className='button button3' onClick={props.onClick}>{props.title}</button>
       );
   }
-  const BLACKBUTTON = () => {
-    return 0;
-  }
-export {GREENBUTTON,REDBUTTON,BLACKBUTTON}
